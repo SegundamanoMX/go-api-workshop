4b119120e09f9a8e7f83b6525385f32a0bd78406
David T. Goris <david.tovar@schibsted.com.mx>
2016-02-16 22:32:51 -0600

    Add query + post form params
    Now we have a post endpoint that uses params in the URL path,
    querystring parameters and parameters in the post form. This
    example works for having a more down to earth POST request.

--- server.go
+++ server.go
@@ -1,19 +1,44 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+    "fmt"
+    "net/http"
+    "github.com/gin-gonic/gin" 
+)
+
 
 func main() {
     router := gin.Default()
 
     v1 := router.Group("/v1")
     {
+        v1.GET("/hello/:name", getHello)
         v1.GET("/hello", getHello)
+        v1.POST("/user/:id", postUser)
     }
     router.Run() // listen and server on 0.0.0.0:8080
 }
 
+func postUser(c *gin.Context) {
+    id := c.Param("id") 
+    other := c.DefaultQuery("page", "0") // shortcut for c.Request.URL.Query().Get("page")
+    message := c.DefaultPostForm("message", "Nothing over here")
+    
+    str := fmt.Sprintf("Id: %s, Message: %s, Other: %s", id, message, other)
+    c.JSON(http.StatusOK, gin.H{
+        "message": str,
+    })
+}
+
 func getHello(c *gin.Context) {
-    c.JSON(200, gin.H{
-        "message": "Hello, World!",
+    var str string
+    name := c.Param("name")
+    if name != "" {
+        str = fmt.Sprint("Hello, ", name)
+    } else {
+        str = fmt.Sprint("Hello, World!")
+    }
+    c.JSON(http.StatusOK, gin.H{
+        "message": str,
     })
 }
