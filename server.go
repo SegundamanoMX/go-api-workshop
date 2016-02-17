73a2c5214752f58f8110a34e22585772d1847620
David T. Goris <david.tovar@schibsted.com.mx>
2016-02-17 10:31:28 -0600

    Add POST endpoint for knowing when user is italian
    Now the API has a POST endpoint that in some particular cases it
    can detect when the user is Italian.

--- server.go
+++ server.go
@@ -6,6 +6,10 @@ import (
     "github.com/gin-gonic/gin" 
 )
 
+type User struct {
+    Name string `json:"name"`
+    Password string `json:"pass"`
+}
 
 func main() {
     router := gin.Default()
@@ -14,12 +18,16 @@ func main() {
     {
         v1.GET("/hello/:name", getHello)
         v1.GET("/hello", getHello)
-        v1.POST("/user/:id", postUser)
+        v1.PUT("/user/:id", putUser)
+    }
+    v2 := router.Group("/v2")
+    {
+        v2.POST("/user", postUser)
     }
     router.Run() // listen and server on 0.0.0.0:8080
 }
 
-func postUser(c *gin.Context) {
+func putUser(c *gin.Context) {
     id := c.Param("id") 
     other := c.DefaultQuery("page", "0") // shortcut for c.Request.URL.Query().Get("page")
     message := c.DefaultPostForm("message", "Nothing over here")
@@ -28,6 +36,19 @@ func postUser(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
         "message": str,
     })
+}
+
+func postUser(c *gin.Context) {
+    var user User
+    
+    if c.BindJSON(&user) == nil {
+        fmt.Println(user)
+        if user.Name == "sergio" && user.Password == "pizza" {
+            c.JSON(http.StatusOK, gin.H{"status": "you are italian"})
+        } else {
+            c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+        }
+    }
 }
 
 func getHello(c *gin.Context) {
