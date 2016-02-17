4e4b77181550721940eea909f901571c7f42d373
David T. Goris <david.tovar@schibsted.com.mx>
2016-02-16 21:50:38 -0600

    Add grouping to the endpoints
    Add groups to the endpoint and split the handlers into functions.

--- server.go
+++ server.go
@@ -3,11 +3,17 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-    r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
+    router := gin.Default()
+
+    v1 := router.Group("/v1")
+    {
+        v1.GET("/hello", getHello)
+    }
+    router.Run() // listen and server on 0.0.0.0:8080
+}
+
+func getHello(c *gin.Context) {
+    c.JSON(200, gin.H{
+        "message": "Hello, World!",
     })
-    r.Run() // listen and server on 0.0.0.0:8080
 }
