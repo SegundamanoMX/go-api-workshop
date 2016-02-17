fcb641b467ac0f4efcb956a3a7b51c0a3d779a59
David T. Goris <david.tovar@schibsted.com.mx>
2016-02-17 12:41:20 -0600

    Use gofmt for formatting.
    The indentation was incorrect for the test file.

--- server_test.go
+++ server_test.go
@@ -1,62 +1,62 @@
 package main
 
 import (
-    _"bytes"
-    _"fmt"
-    "github.com/gin-gonic/gin" 
-    "github.com/stretchr/testify/assert"
-    "net/http"
-    "net/http/httptest"
-    "strings"
-    "testing"
+	_ "bytes"
+	_ "fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
 
 var router *gin.Engine
 
-func initAPI(){
-    router = gin.New()
-    router.POST("/user", postUser) 
-    router.GET("/hello", getHello) 
-    router.GET("/hello/:name", getHello) 
+func initAPI() {
+	router = gin.New()
+	router.POST("/user", postUser)
+	router.GET("/hello", getHello)
+	router.GET("/hello/:name", getHello)
 }
 
 func TestPostUserReturnsWithStatusOK(t *testing.T) {
-    initAPI()
-    data := `{"name": "sergio", "pass":"pizza"}`
-    reader := strings.NewReader(data)
-    request, _ := http.NewRequest("POST", "/user", reader)
-    resp := httptest.NewRecorder()
-    router.ServeHTTP(resp, request)
-    expected := `{"status":"you are italian"}
+	initAPI()
+	data := `{"name": "sergio", "pass":"pizza"}`
+	reader := strings.NewReader(data)
+	request, _ := http.NewRequest("POST", "/user", reader)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, request)
+	expected := `{"status":"you are italian"}
 `
-    assert.Equal(t, resp.Body.String(), expected)
+	assert.Equal(t, resp.Body.String(), expected)
 }
 
 func TestPostUserReturnsWithUnauthorized(t *testing.T) {
-    data := `{"name": "sergio", "pass":"tacos"}`
-    reader := strings.NewReader(data)
-    request, _ := http.NewRequest("POST", "/user", reader)
-    resp := httptest.NewRecorder()
-    router.ServeHTTP(resp, request)
-    expected := `{"status":"unauthorized"}
+	data := `{"name": "sergio", "pass":"tacos"}`
+	reader := strings.NewReader(data)
+	request, _ := http.NewRequest("POST", "/user", reader)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, request)
+	expected := `{"status":"unauthorized"}
 `
-    assert.Equal(t, resp.Body.String(), expected)
+	assert.Equal(t, resp.Body.String(), expected)
 }
 
 func TestGetUserReturnsWithStatusOK(t *testing.T) {
-    request, _ := http.NewRequest("GET", "/hello", nil)
-    resp := httptest.NewRecorder()
-    router.ServeHTTP(resp, request)
-    expected := `{"message":"Hello, World!"}
+	request, _ := http.NewRequest("GET", "/hello", nil)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, request)
+	expected := `{"message":"Hello, World!"}
 `
-    assert.Equal(t, resp.Body.String(), expected)
+	assert.Equal(t, resp.Body.String(), expected)
 }
 
 func TestGetUserNameReturnsWithStatusOK(t *testing.T) {
-    request, _ := http.NewRequest("GET", "/hello/david", nil)
-    resp := httptest.NewRecorder()
-    router.ServeHTTP(resp, request)
-    expected := `{"message":"Hello, david"}
+	request, _ := http.NewRequest("GET", "/hello/david", nil)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, request)
+	expected := `{"message":"Hello, david"}
 `
-    assert.Equal(t, resp.Body.String(), expected)
+	assert.Equal(t, resp.Body.String(), expected)
 }
